593cac05c03604cd0b4f53e615088815e15352fe
arielbk <buchwald.kearney@gmail.com>
2020-12-16 14:49:31 +0100

    fix: EbMin scale notes

--- src/components/Scales.js
+++ src/components/Scales.js
@@ -37,7 +37,7 @@ const scales = {
   'F♯min': '^F2 ^G2 A2 B2 ^c2 d2 e2 ^f2',
   'C♯min': '^C2 ^D2 E2 ^F2 ^G2 A2 B2 ^c2',
   'G♯min': '^G,2 ^A,2 B,2 ^C2 ^D2 E2 ^F2 ^G2',
-  'E♭min': '^D2 ^E2 ^F2 ^G2 ^A2 B2 ^c2 ^d2',
+  'E♭min': '_E2 F2 _G2 _A2 _B2 _c2 _d2 _e2',
   'B♭min': '_B,2 C2 _D2 _E2 F2 _G2 _A2 _B2',
   Fmin: 'F2 G2 _A2 _B2 c2 _d2 _e2 f2',
   Cmin: 'C2 D2 _E2 F2 G2 _A2 _B2 c2',
